49f5539a85dba25de93daeda1c6856eafb7a6519
rohan <rohan.vagadiya14@gmail.com>
2022-12-24 21:30:58 +0530

    create form

--- pages/Contact_me.py
+++ pages/Contact_me.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.title('Contact Me')
+with st.form(key='form'):
+    email_add = st.text_input(label='Your Email:')
+    message= st.text_area('Your message:')
+    submit = st.form_submit_button('submit')
