109fddecabebe24d85d03c4054d89c3ab1f176c8
rohan <rohan.vagadiya14@gmail.com>
2022-12-24 13:40:21 +0530

    add description and photo.

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ import streamlit as st
 st.set_page_config(layout='wide')
 col1, col2 = st.columns(2)
 with col1:
-    st.image('images/photo.png')
+    st.image('images/photo.jpeg')
 
 with col2:
     st.title('Rohan Vagadiya')
     content = """
-    hi rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
-    rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
+    Hi! I am Rohan and currently a student in 11th grade at the new tulip International school. I know HTML5,
+    CSS3, Javascript and python. I love to read books and play squash.
     """
     st.info(content)
 content2 = """
-Below you can find some apps I have built in pytho. Feel free to contact me!
+Below you can find some projects I have built. Feel free to contact me!
 """
+
 st.write(content2)
