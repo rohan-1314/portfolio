28abe373e515dd4e51f955217a52546292021a40
rohan <rohan.vagadiya14@gmail.com>
2022-12-24 14:25:16 +0530

    add thumbnail titles in 2 rows, add description,imgs,url.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 st.set_page_config(layout='wide')
 col1, col2 = st.columns(2)
+import pandas as pd
+
 with col1:
     st.image('images/photo.jpeg')
 
@@ -15,5 +17,20 @@ with col2:
 content2 = """
 Below you can find some projects I have built. Feel free to contact me!
 """
-
+df = pd.read_csv("data.csv", sep=';')
 st.write(content2)
+col3,empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image('images/' + row['image'])
+        st.write(f"[source code]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image('images/' + row['image'])
+        st.write(f"[source code]({row['url']})")
