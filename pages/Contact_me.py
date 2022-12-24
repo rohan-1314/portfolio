59727fdf07df68cc003f3b7077d9ee60d7305948
rohan <rohan.vagadiya14@gmail.com>
2022-12-24 22:44:57 +0530

    send emails using the form

--- pages/Contact_me.py
+++ pages/Contact_me.py
@@ -1,7 +1,17 @@
 import streamlit as st
+from send_email import send_email
 
 st.title('Contact Me')
 with st.form(key='form'):
-    email_add = st.text_input(label='Your Email:')
-    message= st.text_area('Your message:')
+    user_email = st.text_input(label='Your Email:')
+    raw_message = st.text_area('Your message:')
+    message = f"""\
+subject:new email from {user_email} 
+
+from: {user_email}
+{raw_message}
+"""
     submit = st.form_submit_button('submit')
+    if submit:
+        send_email(message)
+        st.info('Your email was sent successfully.')
--- send_email.py
+++ send_email.py
@@ -1,19 +1,16 @@
 import smtplib, ssl
 
-password = 'tycioonycqrosntr'
-username = 'rohan.vagadiya14@gmail.com'
-host = 'smtp.gmail.com'
-port = 465
-reciever = 'adesarapriyal544@gmail.com'
-context = ssl.create_default_context()
-message = """\
-subject:yooo!
-I am sending this email from python.
-So Rad!
 
-"""
-# we did context=context because the func has multiple args and context
-# comes later and host, port are 1st,2nd, so they are in order
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, reciever, message)
+def send_email(message):
+    password = 'tycioonycqrosntr'
+    username = 'rohan.vagadiya14@gmail.com'
+    host = 'smtp.gmail.com'
+    port = 465
+    reciever = 'rohan.vagadiya14@gmail.com'
+    context = ssl.create_default_context()
+
+    # we did context=context because the func has multiple args and context
+    # comes later and host, port are 1st,2nd, so they are in order
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, reciever, message)
