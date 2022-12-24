2990b3cfc3fb193dd23f32fd6b400b7e4982264f
rohan <rohan.vagadiya14@gmail.com>
2022-12-24 23:32:11 +0530

    create secure password using env variables

--- send_email.py
+++ send_email.py
@@ -1,9 +1,10 @@
 import smtplib, ssl
+import os
 
 
 def send_email(message):
-    password = 'tycioonycqrosntr'
     username = 'rohan.vagadiya14@gmail.com'
+    password = os.getenv("PASSWORD")
     host = 'smtp.gmail.com'
     port = 465
     reciever = 'rohan.vagadiya14@gmail.com'
@@ -13,4 +14,4 @@ def send_email(message):
     # comes later and host, port are 1st,2nd, so they are in order
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
-        server.sendmail(username, reciever, message)
+        server.sendmail(username,  reciever, message)
